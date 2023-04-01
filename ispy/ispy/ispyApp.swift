f0f8b4a27a3af8f2f879db6bdb9ea2b806c377f8
Amelia Lane <amelia.lane@duke.edu>
2023-04-01 11:24:31 -0400

    ispy file

---
+++ ispy/ispy/ispyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ispyApp.swift
+//  ispy
+//
+//  Created by Amelia Lane on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ispyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
